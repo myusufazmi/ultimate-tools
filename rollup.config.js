c0d101fe0a95ea78f3e154fbbd36d49d786c414b
Maulana Yusuf Azmi <maulanayusufazmi@gmail.com>
2026-02-09 12:23:53 +0700

    Add Rollup for bundling and minification

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import terser from "@rollup/plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.min.js",
+      format: "esm",
+      plugins: [terser()],
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+  ],
+  plugins: [resolve(), commonjs()],
+};
